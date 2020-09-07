71c79acd4213d0ee0fa989ade237d070aacd4d58
Grigor Tamazyan <grigtm@gmail.com>
2020-09-08 02:00:19 +0400

    edited again

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 import Button from "./Button";
-
+import Name from "../components/Name";
+import Price from "../components/Price";
+import Description from "../components/Description";
 class Product extends Component {
   render() {
+    let {name, price, description} = this.props;
     return (
       <div className="Product">
-        <h2>{this.props.name}</h2>
-        <img src={this.props.img} width="300px"/>
-        <h3>{this.props.price}</h3>
-        <p>{this.props.description}</p>
+        <Name data={name} />
+        <img src={this.props.img} width="300px" alt="img" />
+        <Price data={price} />
+        <Description data={description} />
+        <p></p>
         <Button text="buy now" />
       </div>
     );
